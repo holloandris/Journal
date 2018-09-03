c9f22770b1a26c09616f1870ff327e4057486d7a
Andras Hollo <hollo.andris@gmail.com>
2018-09-02 23:35:26 -0700

    Created framework Xcode project

---
+++ Journal/Journal.h
@@ -0,0 +1,19 @@
+//
+//  Journal.h
+//  Journal
+//
+//  Created by Andras Hollo on 2018. 09. 02..
+//  Copyright © 2018. Andras Hollo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Journal.
+FOUNDATION_EXPORT double JournalVersionNumber;
+
+//! Project version string for Journal.
+FOUNDATION_EXPORT const unsigned char JournalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Journal/PublicHeader.h>
+
+
---
+++ JournalTests/JournalTests.swift
@@ -0,0 +1,36 @@
+//
+//  JournalTests.swift
+//  JournalTests
+//
+//  Created by Andras Hollo on 2018. 09. 02..
+//  Copyright © 2018. Andras Hollo. All rights reserved.
+//
+
+import XCTest
+@testable import Journal
+
+class JournalTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
